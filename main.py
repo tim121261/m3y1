2672195df269317b9821fe0611224fcc5060c87f
tim121261 <163296428+tim121261@users.noreply.github.com>
2024-05-13 18:44:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import random
-facts_list = []
+<!DOCTYPE html>
+<html>
+<head>
+    <title> KODLAND </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>знакомство</h1>
+    <p>P.S. как тебе урок?</p>
+    <h2>Зависимость от смартфона называется номофобией, по факту — это страх остаться без телефона, хотя на самом деле за этим страхом скрывается угроза остаться без интернета. Потому как именно он обеспечивает доступ ко многим ресурсам. Чаще всего зависимость связана именно с контролем импульсов, навязчивостью при онлайн-играх, с онлайн-шопоголизмом.</h2>
+             <ul>
+                   <li>Так, что учись!!!!!!</li>
+                   <li>Пока!</li>
+             </ul>
+             
+             <iframe src="https://vc.ru/flood/701886-100-interesnyh-faktov-obo-vsem-na-svete" target="<"random_fact> Посмотреть случайный факт!</a></a></div>
+            
+    <img src = "C:\Users\Admin\Downloads\2024-04-26_18-07-33.png" alt="Image 1">
+    <img src = "https://sun1-95.userapi.com/s/v1/ig2/ksppPX9e3RyQc2HhyJNANVosKRYSdxi5gGDa0XfD1C9vJcaNGm6d97ncY7nnXXcdA_yUYq5HUJcvaZ3vz8mVgyPA.jpg?size=400x400&quality=96&crop=60,60,483,483&ava=1" alt="Image 2 ">
+    <img src = "C:\Users\Admin\Downloads\2024-04-26_19-52-52.png" alt="Image 3">
+    <iframe src="file:///C:/Users/Admin/Desktop/VS/2%20saqt/index.html" target="_blank
+    
+    
+    " rel="noopener">meteoblue</a></div>
 
-from flask import Flask
+    
+</body>    
 
-
-app = Flask(__name__)
-@app.route("/ПУТЬ")
-def НАЗВАНИЕ_ФУНКЦИИ():
-
-
-@app.route("/")
-def hello_world():
-
-    return '<h1>Hello, World!</h1>'
-
-return f'<p>{random.choice(facts_list)}</p>'
-
-
-app.run(debug=True)
+</html>
